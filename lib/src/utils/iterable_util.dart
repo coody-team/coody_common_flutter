78c50a5570dda046b5e37d76e6aa4ce45db3d82a
Psunu <psw9255439@naver.com>
2024-01-01 16:07:12 +0900

    feat: Iterable 유틸리티 메소드 추가
    - containsWhere
    - isSameWith

--- lib/src/utils/iterable_util.dart
+++ lib/src/utils/iterable_util.dart
@@ -1,6 +1,8 @@
+import 'package:collection/collection.dart';
+
 extension IterableX<T> on Iterable<T> {
   /// [other]에 속한 항목 중 한 개라도 포함되어 있으면 true
-  containsOneOf(Iterable<T> other) {
+  bool containsOneOf(Iterable<T> other) {
     for (final e in other) {
       if (contains(e)) return true;
     }
@@ -8,10 +10,24 @@ extension IterableX<T> on Iterable<T> {
   }
 
   /// [other]에 속하지 않는 항목을 포함하고 있으면 true
-  containsNotIn(Iterable<T> other) {
+  bool containsNotIn(Iterable<T> other) {
     final otherSet = Set.from(other);
     final thisSet = Set.from(this);
 
     return thisSet.difference(otherSet).isNotEmpty;
+  }
+
+  bool containsWhere(bool Function(T element) test) {
+    return firstWhereOrNull(test) != null;
+  }
+
+  bool isSameWith(Iterable<T> other) {
+    if (identical(this, other)) return true;
+    if (length != other.length) return false;
+
+    return foldIndexed(true, (index, previous, element) {
+      if (!previous) return false;
+      return element == other.elementAt(index);
+    });
   }
 }
